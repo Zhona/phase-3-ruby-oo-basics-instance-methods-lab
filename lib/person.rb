a971c3bd9dba8189083fdc505cae779e2c010ee4
Teana Zhong <Teanawk@gmail.com>
2022-11-01 23:21:01 -0400

    commit tree 659eaf2fa3a865458d99e811fe66f06b2b402951

---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+class Person 
+    def talk 
+        puts "Hello, World!"
+    end
+
+    def walk 
+        puts "The Person is walking"
+    end
+end 
+
+teana = Person.new 
+teana.talk 
+teana.walk
